1a696d4e978fcf9f08aa37cba0d0e12f35a33c66
Christopher McComas <cmccomas@marshall.edu>
2021-05-19 14:08:33 -0400

    wip

--- header.php
+++ header.php
@@ -15,6 +15,13 @@
 <html class="no-js" <?php language_attributes(); ?>>
 
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WRVRH87');</script>
+<!-- End Google Tag Manager -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
@@ -305,6 +312,11 @@
 </head>
 
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WRVRH87"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
     <div>
         <div style="padding: 1.5rem; width: 100%; background: #000;">
             <div style="max-width: 85rem!important; margin-right: auto;
